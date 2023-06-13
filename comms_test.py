02296349dd022ff5231ff30a822b8e2d69ca6f9c
Luis Ramirez <luis.ramirezsolis@cinvestav.mx>
2023-06-12 20:42:57 -0700

    docker files created and stream test ok

--- comms_test.py
+++ comms_test.py
@@ -1,11 +1,11 @@
 import asyncio
-
+import json
 from azure.eventhub import EventData
 from azure.eventhub.aio import EventHubProducerClient
 from azure.identity.aio import DefaultAzureCredential
 
 EVENT_HUB_FULLY_QUALIFIED_NAMESPACE = "coffe-events.servicebus.windows.net"
-EVENT_HUB_NAME = "coffe-stream"
+EVENT_HUB_NAME = "coffee-stream"
 
 credential = DefaultAzureCredential()
 
@@ -23,15 +23,33 @@ async def run():
         # Create a batch.
         event_data_batch = await producer.create_batch()
 
+        data_first = {
+            "serial_number": "3",
+            "model": "top",
+            "event_date": "12062023",
+            "event_type": {
+                "Clean": "Success"
+            }
+        }
+        data_second = {
+            "serial_number": "4",
+            "model": "low",
+            "event_date": "12062023",
+            "event_type": {
+                "Brew": "Success"
+            }
+        }
+
         # Add events to the batch.
-        event_data_batch.add(EventData("First event "))
-        event_data_batch.add(EventData("Second event"))
-        event_data_batch.add(EventData("Third event"))
+        event_data_batch.add(EventData(json.dumps(data_first)))
+        event_data_batch.add(EventData(json.dumps(data_second)))
 
         # Send the batch of events to the event hub.
         await producer.send_batch(event_data_batch)
 
         # Close credential when no longer needed.
         await credential.close()
+
+        print("Data sent successfully")
 
 asyncio.run(run())
